cb9025263009e8b65e6bdae05f1a67da350d9ec3
Suz's laptop <45858103+gelid-ganef@users.noreply.github.com>
2024-08-05 20:03:18 -0400

    composed header into component

--- components.js
+++ components.js
@@ -1,14 +1,27 @@
-fetch()
 class TopBanner extends HTMLElement {
     constructor() {
         super();
     }
     connectedCallback() {
-        for (let a of this.getElementsByTagName("a")) {
-            if (a.getAttribute("href") == window.location.pathname) {
-                a.setAttribute("class", "currentPage");
-            }
-        }
+        this.innerHTML = `
+<!--TOP-BANNER web component-->
+    <link rel="stylesheet" href="./components/bannerstyle.css">
+    <div id=banner>
+        <header>
+            <h1><img src="./assets/logo_lg.png" ; alt="All In!" ; height=200 />
+                <slot class="sub">events in RVA</slot>
+            </h1>
+        </header>
+        <div id=navbar><nav>
+            <ul class="sideways">
+                <li><a class="current" href="/index.html">home</a></li>
+                <li><a href="/about.html">about us</a></li>
+                <li><a href="/blog.html">stories</a></li>
+                <li><a href="/events.html">plan an event</a></li>
+                <li><a href="/volunteer.html">get involved</a></li>
+            </ul>
+        </nav></div>
+    </div>`
     }
 }
-customElements.define("top-banner", MyAutonomousElement);
+customElements.define("top-banner", TopBanner);
